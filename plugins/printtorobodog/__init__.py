e4a357f31b5fd9e2d5dac527bddc4ff8c9c0fe8d
Franqy006 <michalff1@o2.pl>
2021-11-14 16:36:10 +0100

    try to connect to other program

--- plugins/printtorobodog/__init__.py
+++ plugins/printtorobodog/__init__.py
@@ -1,4 +1,21 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+from multiprocessing.connection import Client
+
+address = ('localhost', 6000)
+conn = Client(address, authkey='secret password')
+j=0
+while j<3:
+    conn.send('elooo')
+    j=j+1;
+# can also send arbitrary objects:
+# conn.send(['a', 2.5, None, int, sum])
+# conn.send('close')
+# conn.close()
+
+
+
 def run(readable_results, data, rawbuf):
-    print "test"
+    conn.send('readable_results')
+    print "results sent to listener"
+
